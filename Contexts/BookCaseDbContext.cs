869b1df93fb877e61799093ba87aff6fa24d7db2
NeshGogo <rafaelaguero10@hotmail.com>
2021-03-23 23:18:13 -0400

    init

---
+++ Contexts/BookCaseDbContext.cs
@@ -0,0 +1,20 @@
+﻿using bookcaseApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookcaseApi.Contexts
+{
+    public class BookCaseDbContext: DbContext
+    {
+        public BookCaseDbContext(DbContextOptions<BookCaseDbContext> options) 
+            : base(options)  
+        {
+
+        }
+
+        public DbSet<Author> Authors { get; set; }
+    }
+}
---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,26 @@
+﻿using bookcaseApi.Contexts;
+using bookcaseApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookcaseApi.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class AuthorsController: ControllerBase
+    {
+        private readonly BookCaseDbContext _context;
+
+        public AuthorsController(BookCaseDbContext context)
+        {
+            _context = context;
+        }
+        public ActionResult<IEnumerable<Author>> Get()
+        {
+            return _context.Authors.ToList();
+        }
+    }
+}
