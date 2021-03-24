513b867b6f15634dc0a2c3f8b32c53d3c4692cc9
NeshGogo <rafaelaguero10@hotmail.com>
2021-03-24 00:08:17 -0400

    author controller complete

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -1,6 +1,7 @@
 ﻿using bookcaseApi.Contexts;
 using bookcaseApi.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,9 +19,50 @@ namespace bookcaseApi.Controllers
         {
             _context = context;
         }
+        [HttpGet]
         public ActionResult<IEnumerable<Author>> Get()
         {
             return _context.Authors.ToList();
+        }
+        [HttpGet("{id}", Name = "GetAuthor")]
+        public ActionResult<Author> Get(int id )
+        {
+            var author = _context.Authors.FirstOrDefault(a => a.Id == id);
+            
+            if(author == null)          
+                return NotFound();
+           
+            return author;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Author author)
+        {
+            _context.Add(author);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("GetAuthor", new { id = author.Id }, author);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id,[FromBody] Author author)
+        {
+            if (id != author.Id)
+                return BadRequest();
+
+            _context.Entry(author).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public ActionResult<Author> Delete(int id)
+        {
+            var author = _context.Authors.FirstOrDefault(a => a.Id == id);
+            if (author == null)
+                return NotFound();
+
+            _context.Authors.Remove(author);
+            _context.SaveChanges();
+            return author;
         }
     }
 }
