13b3a7672365c6a10cf307e25aff04cc5222efdd
NeshGogo <rafaelaguero10@hotmail.com>
2021-03-26 22:50:51 -0400

    custom validation

--- Entities/Author.cs
+++ Entities/Author.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using bookcaseApi.helpers;
 
 namespace bookcaseApi.Entities
 {
-    public class Author
+    public class Author: IValidatableObject
     {
         public int Id { get; set; }
         [Required]
+        /*[FirstLetterUpperCase]*/
         public string Name { get; set; }
         public List<Book> Books { get; set; }
+
+        // Asi es como se hace la validacion del modelo completo.
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var firstLetter = !string.IsNullOrEmpty(Name) ? Name[0].ToString() : null;
+
+            if (firstLetter != null && firstLetter != firstLetter.ToUpper())
+            {
+                yield return new ValidationResult("The first letter must be upper case", new string[] { nameof(Name)});
+            }
+        }
     }
 }
---
+++ helpers/FirstLetterUpperCaseAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookcaseApi.helpers
+{
+    public class FirstLetterUpperCaseAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            // Avoid validate others responsibilities.
+            if(value == null)
+            {
+                return ValidationResult.Success;
+            }
+
+            var firstLetter = value.ToString()[0].ToString();
+            
+            if(firstLetter != firstLetter.ToUpper())
+            {
+                return new ValidationResult("The first letter must be upper case");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
